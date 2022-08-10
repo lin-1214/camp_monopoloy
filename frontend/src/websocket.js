d10f1d90e3c653e8af356ec2a2da4203f3145ffe
hackhaha1 <hankchui88@gmail.com>
2022-08-10 18:27:15 +0800

    websocket init

---
+++ frontend/src/websocket.js
@@ -0,0 +1,8 @@
+import React from "react";
+import io from "socket.io-client";
+import { usedispatch } from "react-redux";
+
+const WEBSOCKET_URL = "localhost:5000";
+
+export const socket = io(WEBSOCKET_URL);
+export const SocketContext = React.createContext(socket);
