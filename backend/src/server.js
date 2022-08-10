731ea248aa48ad9a0920a055f17904463a215bbf
hackhaha1 <hankchui88@gmail.com>
2022-08-10 17:56:24 +0800

    socket backend preview

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,9 @@ import http from "http";
 import mongoose from "mongoose";
 import dotenv from "dotenv-defaults";
 import express from "express";
+import Land from "../models/land.js";
+import Team from "../models/team.js";
+import User from "../models/user.js";
 const app = express();
 const server = http.createServer(app);
 
@@ -22,12 +25,45 @@ mongoose.connect(process.env.MONGO_URL, {
   useUnifiedTopology: true,
 });
 
+async function updateLand() {
+  await Land.find({})
+    .exec()
+    .then((data) => {
+      io.emit("updateLand", data);
+    })
+    .catch((e) => console.error(e));
+}
+
+async function updateTeam() {
+  await Team.find({})
+    .exec()
+    .then((data) => {
+      io.emit("updateTeam", data);
+    })
+    .catch((e) => console.error(e));
+}
+
+async function updateUser() {
+  await User.find({})
+    .exec()
+    .then((data) => {
+      io.emit("updateUser", data);
+    })
+    .catch((e) => console.error(e));
+}
+
 db.once("open", () => {
   console.log("MongoDB connected");
   io.on("connection", (socket) => {
     console.log("a user connected");
     socket.on("getMessage", (arg) => {
       console.log(arg);
+    });
+    updateLand();
+    updateTeam();
+    updateUser();
+    socket.on("disconnect", (socket) => {
+      console.log("a user disconnected");
     });
   });
   const PORT = process.env.port || 5000;
